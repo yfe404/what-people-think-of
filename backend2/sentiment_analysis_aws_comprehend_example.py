140744d389e74751eb981240f99988d9d1e90447
yafeunteun <yannfeunteun@gmail.com>
2018-10-14 18:43:00 +0200

    test with another sentence

--- backend2/sentiment_analysis_aws_comprehend_example.py
+++ backend2/sentiment_analysis_aws_comprehend_example.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import boto3
 client = boto3.client('comprehend')
-response = client.detect_sentiment(Text="I love sushis", LanguageCode="en")
+response = client.detect_sentiment(Text="John Doe is angry that he never gets good matches on Tinder", LanguageCode="en")
 response
 print(response["Sentiment"], response["SentimentScore"])
